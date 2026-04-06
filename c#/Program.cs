12a6e138ff9f7ffde6600c50404528e03fdefac9
gcastanom <gcastano@unicartagena.edu.co>
2026-04-05 22:19:51 -0500

    Inicializando repositorio

---
+++ c#/Program.cs
@@ -0,0 +1,9 @@
+﻿using recordExample;
+
+var p1 = new Point(1, 2);
+var p2 = new Point(1, 2);
+var c1 = new Circle(p1, 5);
+
+Console.WriteLine(p1);
+Console.WriteLine(c1);
+Console.WriteLine("p1 es igual a p2: {0}", p1 == p2);
---
+++ c#/record.cs
@@ -0,0 +1,5 @@
+namespace recordExample;
+
+public record Point(int X, int Y);
+
+public record Circle(Point Center, int Radius);
